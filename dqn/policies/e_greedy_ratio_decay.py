e5487d36fc20814e7f330b09c0ca8b4984bed469
ilvieira <ines.l.vieira@tecnico.ulisboa.pt>
2021-10-22 15:57:47 +0100

    New policy added

--- dqn/policies/e_greedy_ratio_decay.py
+++ dqn/policies/e_greedy_ratio_decay.py
@@ -1,20 +1,21 @@
 from dqn.policies.e_greedy import EGreedy
 
 
-class EGreedyLinearDecay(EGreedy):
-    """ Epsilon-greedy policy with linear decay."""
+class EGreedyRatioDecay(EGreedy):
+    """ Epsilon-greedy policy whose epsilon decays to r*epsilon every time.
+    decay should be a number between 0 and 1."""
 
-    def __init__(self, epsilon=1, min_epsilon=0.1, steps_of_decay=1_000_000, n_actions=None):
+    def __init__(self, epsilon=1, min_epsilon=0.1, ratio_of_decay=0.995, n_actions=None):
         super().__init__(epsilon, n_actions=n_actions)
-        self.decay = (epsilon-min_epsilon)/steps_of_decay
+        self.decay = ratio_of_decay
         self.min_epsilon = min_epsilon
 
     def choose_action(self, Q=None):
         """Q is an tensor with shape (N,) or an ndarray, containing the Q values of the N possible actions"""
-        action_index = super(EGreedyLinearDecay, self).choose_action(Q=Q)
+        action_index = super().choose_action(Q=Q)
 
         # update the epsilon in the initial decay phase
         if self.epsilon > self.min_epsilon:
-            self.epsilon -= self.decay
+            self.epsilon *= self.decay
 
         return action_index
